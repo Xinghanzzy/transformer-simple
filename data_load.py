e6f245db341ce1744ece516104982e9ab50a4d5b
Xinghanzzy <hldnpqzzy@sina.com>
2019-03-10 12:01:53 +0800

    fisrt commit

---
+++ data_load.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+#/usr/bin/python2
+'''
+June 2017 by kyubyong park. 
+kbpark.linguist@gmail.com.
+https://www.github.com/kyubyong/transformer
+'''
+from __future__ import print_function
+from hyperparams import Hyperparams as hp
+import tensorflow as tf
+import numpy as np
+import codecs
+import regex
+
+def load_de_vocab():
+    vocab = [line.split()[0] for line in codecs.open('preprocessed/de.vocab.tsv', 'r', 'utf-8').read().splitlines() if int(line.split()[1])>=hp.min_cnt]
+    word2idx = {word: idx for idx, word in enumerate(vocab)}
+    idx2word = {idx: word for idx, word in enumerate(vocab)}
+    return word2idx, idx2word
+
+def load_en_vocab():
+    vocab = [line.split()[0] for line in codecs.open('preprocessed/en.vocab.tsv', 'r', 'utf-8').read().splitlines() if int(line.split()[1])>=hp.min_cnt]
+    word2idx = {word: idx for idx, word in enumerate(vocab)}
+    idx2word = {idx: word for idx, word in enumerate(vocab)}
+    return word2idx, idx2word
+
+def create_data(source_sents, target_sents): 
+    de2idx, idx2de = load_de_vocab()
+    en2idx, idx2en = load_en_vocab()
+    
+    # Index
+    x_list, y_list, Sources, Targets = [], [], [], []
+    for source_sent, target_sent in zip(source_sents, target_sents):  #应该是for循环取出每一个句子
+        x = [de2idx.get(word, 1) for word in (source_sent + u" </S>").split()] # 1: OOV, </S>: End of Text
+        y = [en2idx.get(word, 1) for word in (target_sent + u" </S>").split()]  #返回指定键的值，如果值不在字典中返回 1
+        if max(len(x), len(y)) <=hp.maxlen:
+            x_list.append(np.array(x))
+            y_list.append(np.array(y))
+            Sources.append(source_sent)
+            Targets.append(target_sent)
+    
+    # Pad      
+    X = np.zeros([len(x_list), hp.maxlen], np.int32) # len * 10 matrix
+    Y = np.zeros([len(y_list), hp.maxlen], np.int32)
+    for i, (x, y) in enumerate(zip(x_list, y_list)):
+        X[i] = np.lib.pad(x, [0, hp.maxlen-len(x)], 'constant', constant_values=(0, 0)) #补全
+        Y[i] = np.lib.pad(y, [0, hp.maxlen-len(y)], 'constant', constant_values=(0, 0)) #x和y后面pad上x和y初始元素个数和句子最大长度差的那么多数值 0
+    
+    return X, Y, Sources, Targets
+
+def load_train_data():
+    de_sents = [regex.sub("[^\s\p{Latin}']", "", line) for line in codecs.open(hp.source_train, 'r', 'utf-8').read().split("\n") if line and line[0] != "<"]
+    en_sents = [regex.sub("[^\s\p{Latin}']", "", line) for line in codecs.open(hp.target_train, 'r', 'utf-8').read().split("\n") if line and line[0] != "<"]
+    
+    X, Y, Sources, Targets = create_data(de_sents, en_sents)
+    return X, Y
+    
+def load_test_data():
+    def _refine(line): # vt. 精炼，提纯；改善；使…文雅
+        line = regex.sub("<[^>]+>", "", line)    # 去xml标签
+        line = regex.sub("[^\s\p{Latin}']", "", line)  # 空格等不可见字符 和 拉丁字母 以外的被删除
+        return line.strip()
+    
+    de_sents = [_refine(line) for line in codecs.open(hp.source_test, 'r', 'utf-8').read().split("\n") if line and line[:4] == "<seg"]
+    en_sents = [_refine(line) for line in codecs.open(hp.target_test, 'r', 'utf-8').read().split("\n") if line and line[:4] == "<seg"]
+        
+    X, Y, Sources, Targets = create_data(de_sents, en_sents)
+    return X, Sources, Targets # (1064, 150)
+
+def get_batch_data():
+    # Load data
+    X, Y = load_train_data()
+    
+    # calc total batch count
+    num_batch = len(X) // hp.batch_size
+    
+    # Convert to tensor
+    X = tf.convert_to_tensor(X, tf.int32)
+    Y = tf.convert_to_tensor(Y, tf.int32)
+    
+    # Create Queues
+    input_queues = tf.train.slice_input_producer([X, Y])
+            
+    # create batch queues
+    x, y = tf.train.shuffle_batch(input_queues,
+                                num_threads=8,
+                                batch_size=hp.batch_size, 
+                                capacity=hp.batch_size*64,   
+                                min_after_dequeue=hp.batch_size*32, 
+                                allow_smaller_final_batch=False)
+    
+    return x, y, num_batch # (N, T), (N, T), ()
+    # x，y的shape为[N,T]，N即batch_size的大小，T为最大句子长度maxlen，默认为10。
+
